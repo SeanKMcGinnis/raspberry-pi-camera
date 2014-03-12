6d972dab0469d4c66c9a9c57aa6a9e62e77d812d
Sean McGinnis <seankmcginnis@gmail.com>
2014-03-12 18:25:47 -0400

    Added comments

--- picamera-time-lapse.py
+++ picamera-time-lapse.py
@@ -2,8 +2,13 @@ import time
 import picamera
 
 with picamera.PiCamera() as camera:
-    camera.start_preview()
+    # Open a preview window if you want to see what the camera is capturing
+	camera.start_preview()
+	# Wait two seconds before capturing images
     time.sleep(2)
+	# Create a file name and capture the image
     for filename in camera.capture_continuous('../ImageOutput/img{counter:03d}.jpg'):
-        print('Captured %s' % filename)
+        # Write to the console the name of the image captured
+		print('Captured %s' % filename)
+		#Wait 5 seconds before capturing the next image
         time.sleep(5) # wait 5 seconds
