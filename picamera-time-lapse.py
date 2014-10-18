4c9a04692601b4d5f2ca84fbc0c6638ab05125df
Sean McGinnis <seankmcginnis@gmail.com>
2014-10-17 20:48:53 -0400

    modify to accept parameters

--- picamera-time-lapse.py
+++ picamera-time-lapse.py
@@ -1,14 +1,18 @@
 import time
 import picamera
 
-with picamera.PiCamera() as camera:
-    # Open a preview window if you want to see what the camera is capturing
-	camera.start_preview()
-	# Wait two seconds before capturing images
-    time.sleep(2)
-	# Create a file name and capture the image
-    for filename in camera.capture_continuous('../ImageOutput/img{counter:03d}.jpg'):
-        # Write to the console the name of the image captured
-		print('Captured %s' % filename)
-		#Wait 5 seconds before capturing the next image
-        time.sleep(5) # wait 5 seconds
+def captureImages(delay, flight_duration, image_interval):
+    
+    camera = picamera.PiCamera()
+    true_interval = image_interval - 3
+    image_count = (flight_duration * 60)/image_interval
+    print 'Going to Capture %s images over %s minutes with %s seconds between images' % (image_count, flight_duration, image_interval)
+    time.sleep(delay)
+    try:
+        for x in range(image_count):
+            timestr = time.strftime("%Y%m%d-%H%M%S")
+            camera.capture('ImageOutput/img' + timestr +'.jpg')
+            time.sleep(true_interval)
+    finally:
+        camera.close()
+        print 'Image capture session ended'
