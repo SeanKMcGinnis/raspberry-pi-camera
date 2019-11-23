61dacf38e96192f74ff302fc474c879470f97d45
Sean McGinnis <seankmcginnis@gmail.com>
2019-11-23 09:01:52 -0500

    General Updates

--- RaspiTimelapseCSV.py
+++ RaspiTimelapseCSV.py
@@ -17,6 +17,7 @@ def captureImages(delay, flight_duration, image_interval):
     csv_filename = datetime.now().strftime("%m_%d_%Y") + "_images.csv"
     with open(csv_filename, 'wb') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',', quotechar="|", quoting=csv.QUOTE_MINIMAL)
+        # To extend the information collected, edit the row below to include the desired field headers
         filewriter.writerow(['photo', 'lat', 'long', 'date'])
         print 'Going to Capture %s images over %s minutes with %s seconds between images' % (image_count, flight_duration, image_interval)
         time.sleep(delay)
@@ -25,6 +26,7 @@ def captureImages(delay, flight_duration, image_interval):
                 timestr = time.strftime("%Y%m%d-%H%M%S")
                 img_filename = 'ImageOutput/img_' + timestr +'.jpg'
                 camera.capture(img_filename)
+                # Include the additional attributes in the row below if desired
                 filewriter.writerow([img_filename, '77.062', '33.872', timestr])
                 time.sleep(true_interval)
         finally:
